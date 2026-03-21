958ec45394e23ed3a5ea98a6b3089ebf1e7e91ee
Levi Adler <levialbuquerque05@gmail.com>
2026-03-21 04:09:49 -0300

    chore(docker,settings): 🐳 otimizar Dockerfile com imagem slim, instalar dependências do sistema e configurar DEFAULT_AUTO_FIELD

--- src/rickpedia/settings.py
+++ src/rickpedia/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rickpedia.wsgi.application'
 
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
