d58807ebd98885a1d0d091a6f1ba65e71af5fa06
Levi Adler <levialbuquerque05@gmail.com>
2026-03-21 06:08:54 -0300

    feat(ui): 🆕 Adicionar modelo index.html para a página inicial com estilo e botões de navegação.

--- src/rickpedia/settings.py
+++ src/rickpedia/settings.py
@@ -1,24 +1,26 @@
 
 import sys
 from pathlib import Path
-
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
+# Load .env file
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pr3_d1)v&50$n5+4y+atu*s*hb!vl84p^(mhxr37!k03_03n4*'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
